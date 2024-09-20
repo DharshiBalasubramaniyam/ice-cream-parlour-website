b6e1404774501c315af4a2523188be0999e5a95a
dharshib <Dharshib.8@gmail.com>
2024-09-20 15:08:25 +0530

    local storage for cart

--- script/products.js
+++ script/products.js
@@ -1,10 +1,14 @@
 import products_list from "./product_database.js";
 import flavor_list from "./flavor_database.js";
 
+const LOCAL_STORAGE_CART_KEY = "icyco_cart";
+let cartItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_CART_KEY)) || [];
+
 document.addEventListener("DOMContentLoaded", () => {
-        displayFlavorFilter()
-        displayProducts(products_list)
-    }
+    displayFlavorFilter()
+    displayProducts(products_list)
+    displayCartItems()
+}
 )
 
 // display products
@@ -15,7 +19,9 @@ function displayProducts(products) {
     products.forEach(product => {
         let box = document.createElement("div");
         let flavorName = flavor_list.find(f => f.id == product.flavor_id)?.name;
+
         box.setAttribute("class", "box"); box.setAttribute("id", product.id);
+
         box.innerHTML = `<div class="image-wrapper">
                         <div class="off">-${product.off}%</div>
                         <img src="${product.image}" alt="">
@@ -31,10 +37,23 @@ function displayProducts(products) {
                                 <span class="pcs">0</span>
                                 <span class="increase">+</span>
                             </div>
-                            <button class="add-to-cart-btn">Add to Cart</button>`;
+                            `;
+
+
+
+
+        let addToCartButton = document.createElement("button");
+        addToCartButton.textContent = "Add to Cart";
+        addToCartButton.addEventListener("click", (e) => {
+            addToCart(e)
+        })
+
+        box.appendChild(addToCartButton)
 
         productsSection.appendChild(box);
     });
+    handleQuantityButtonsInProductCard()
+
 }
 
 // display flavors filter
@@ -74,26 +93,108 @@ function filterProducts(id) {
     if (id != "all") {
         productsToDisplay = products_list.filter(p => p.flavor_id == id)
     }
-    console.log(productsToDisplay)
 
     displayProducts(productsToDisplay)
 
 }
 
-const increaseBtns = document.querySelectorAll(".increase");
-const decreaseBtns = document.querySelectorAll(".decrease");
+// Add items to cart
+function addToCart(e) {
+    let cartItemId = e.target.parentElement.getAttribute("id");
 
-increaseBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        btn.previousElementSibling.textContent = parseInt(btn.previousElementSibling.textContent) + 1;
-        console.log("incremented");
+    if (cartItems.filter(item => item?.itemId == cartItemId).length != 0) {
+        alert("The item already in your cart!");
+        return;
+    }
+
+    let item = products_list.find(item => item.id == cartItemId);
+    let pcs = e.target.parentElement.querySelector(".pcs").textContent;
+    let amount = item.price * pcs;
+
+    if (pcs == 0) {
+        alert("Please select number of cups you want!");
+        return;
+    }
+
+    cartItems.push({
+        itemId: cartItemId,
+        pcs: pcs,
+        amount: amount
     });
-});
-decreaseBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        if (btn.nextElementSibling.textContent > 0) {
-            btn.nextElementSibling.textContent = parseInt(btn.nextElementSibling.textContent) - 1;
-        }
-        console.log("decremented");
+
+    localStorage.setItem(LOCAL_STORAGE_CART_KEY, JSON.stringify(cartItems))
+    alert(`${pcs} ${item.name} ice cream/s successfully added to the cart!`);
+
+    displayCartItems()
+
+}
+
+function handleRemoveButtonInCart() {
+    let removeBtns = document.querySelectorAll(".remove-cart-item-btn");
+    removeBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            cartItems = cartItems.filter(item => item?.itemId != btn.parentElement.parentElement.getAttribute("id"))
+            localStorage.setItem(LOCAL_STORAGE_CART_KEY, JSON.stringify(cartItems))
+            displayCartItems()
+            if (cartItems.length == 0) {
+                document.querySelector(".empty-cart").classList.add("active")
+                document.querySelector(".no-empty-cart").classList.remove("active")
+            }
+        })
     });
-});
+
+}
+
+function displayCartItems() {
+
+    document.querySelector(".empty-cart").classList.remove("active")
+    document.querySelector(".no-empty-cart").classList.add("active")
+
+    const cartUlList = document.querySelector(".cart-list-items");
+    cartUlList.innerHTML = "";
+
+    let subtotal = 0, noOfItems = 0;
+    cartItems.map(ci => {
+        let itemLi = document.createElement("li");
+
+        itemLi.setAttribute("id", ci.itemId);
+
+        let product = products_list.find(p => p.id == ci.itemId)
+
+        itemLi.innerHTML = `<img src="${product.image}" alt="">
+                            <div class="text">
+                                <span class="name">${product.name}</span><br>
+                                <span class="qty">${ci.pcs}</span> x ${product.price} <br>
+                                <div class="price">$${ci.amount}</div>
+                                <i class="fa fa-times remove-cart-item-btn" aria-hidden="true"></i>
+                            </div>`;
+
+        cartUlList.appendChild(itemLi);
+        handleRemoveButtonInCart()
+        subtotal += Number.parseFloat(ci.amount);
+        noOfItems += Number.parseInt(ci.pcs)
+    })
+
+    document.querySelector(".sub-total").textContent = `$${subtotal}`;
+    document.querySelector(".no-of-cart-items").textContent = noOfItems;
+
+}
+
+function handleQuantityButtonsInProductCard() {
+    const increaseBtns = document.querySelectorAll(".increase");
+    const decreaseBtns = document.querySelectorAll(".decrease");
+
+    increaseBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            btn.previousElementSibling.textContent = parseInt(btn.previousElementSibling.textContent) + 1;
+        });
+    });
+    decreaseBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            if (btn.nextElementSibling.textContent > 0) {
+                btn.nextElementSibling.textContent = parseInt(btn.nextElementSibling.textContent) - 1;
+            }
+        });
+    });
+}
+
