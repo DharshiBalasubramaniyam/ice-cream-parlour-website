919df31499894840668136a2cb7fccc7bdec090a
dharshib <Dharshib.8@gmail.com>
2024-09-20 08:50:15 +0530

    add services section

---
+++ script/flavor_database.js
@@ -0,0 +1,22 @@
+const falvors_list = [
+    {
+        id: "f001",
+        name: "Regular"
+    },
+    {
+        id: "f002",
+        name: "Gelato"
+    },
+    {
+        id: "f003",
+        name: "Sorbet"
+    },
+    {
+        id: "f004",
+        name: "Frozen Yogurt"
+    },
+    {
+        id: "f005",
+        name: "Non Diary/Vegen"
+    }
+]
