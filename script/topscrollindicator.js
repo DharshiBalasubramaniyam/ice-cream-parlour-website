3d2542f1d391aab81177153237090fd8961a7353
srishti <srishtichoudhary52@gmail.com>
2024-10-05 15:15:56 +0530

    Added button to go to top and also scroll progress bar

---
+++ script/topscrollindicator.js
@@ -0,0 +1,7 @@
+window.onscroll = function() {
+    var scrollProgress = document.querySelector('.scroll-progress');
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (scrollTop / scrollHeight) * 100;
+    scrollProgress.style.width = scrolled + "%";
+};
