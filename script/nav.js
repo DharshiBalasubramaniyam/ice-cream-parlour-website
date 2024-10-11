240c5d97f4fff5c55e2aba176c0879d85197d749
Prayag Thakur <114804510+PrAyAg9@users.noreply.github.com>
2024-10-11 22:19:12 +0530

    Update nav.js

--- script/nav.js
+++ script/nav.js
@@ -1,7 +1,53 @@
 // nav.js
+const menu = document.querySelector(".menu");
 // Selecting the links and sections
 const links = document.querySelectorAll(".link");
 const sections = document.querySelectorAll("section");
+
+menu.addEventListener("click", controlMenu)
+
+function controlMenu() {
+    const navlinks = document.querySelector(".nav-links");
+    console.log(navlinks.classList)
+    if (navlinks.classList.contains("active")) {
+        navlinks.classList.remove("active");
+    }
+    else {
+        navlinks.classList.add("active");
+    }
+}
+
+function controlLinks() {
+    const links = document.querySelectorAll(".link");
+    const navlinks = document.querySelector(".nav-links");
+    links.forEach(link => {
+        link.addEventListener("click", ()=> {
+            if (navlinks.classList.contains("active")) {
+                navlinks.classList.remove("active")
+            }
+        })
+    });
+}
+
+controlLinks();
+
+// ================scroll  bottom to top==========================
+const scrollButton = document.querySelector(".scroll-button");
+
+// when the user scrolled then show the button otherwise hide it
+window.addEventListener("scroll", () => {
+  window.pageYOffset > 100
+    ? scrollButton.classList.add("show-btn")
+    : scrollButton.classList.remove("show-btn");
+});
+
+// when user click the button take him to the top with smooth behavior
+scrollButton?.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // for smoothly scrolling
+  });
+});
 
 // Intersection Observer options
 const observerOptions = {
@@ -42,8 +88,6 @@ links.forEach(link => {
         this.classList.add("active");
     });
 });
-
-
 
 window.addEventListener('load', function() {
     setTimeout(function() {
