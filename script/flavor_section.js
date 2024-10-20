6787ff2f6ad43a0d36874d8fc7fd86036f30faa3
dharshib <Dharshib.8@gmail.com>
2024-10-20 15:09:30 +0530

    update

--- script/flavor_section.js
+++ script/flavor_section.js
@@ -1,20 +1,16 @@
-const flavors = [
-  { id: "f001", name: "Regular", image: "images/regular.jpg" },
-  { id: "f002", name: "Gelato", image: "images/gelato.jpg" },
-  { id: "f003", name: "Sorbet", image: "images/sorbet.jpg" },
-  { id: "f004", name: "Yogurt", image: "images/yogurt.jpg" },
-  { id: "f005", name: "Non-Dairy/Vegan", image: "images/vegan.jpg" },
-];
+import flavors from "./flavor_database.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   const flavorContainer = document.querySelector(".flavors-container");
   flavorContainer.innerHTML = flavors
     .map(
       (flavor) => `
-          <div class="flavor-item" onclick="window.location.href='products.html?flavor=${flavor.id}'">
-              <img src="${flavor.image}" alt="${flavor.name}">
-              <h3>${flavor.name}</h3>
-          </div>
+          <a href="products.html?flavor=${flavor.id}">
+              <div class="flavor-item">
+                <img src="${flavor.image}" alt="${flavor.name}">
+                <h3>${flavor.name}</h3>
+              </div>
+          </a>
       `
     )
     .join("");
