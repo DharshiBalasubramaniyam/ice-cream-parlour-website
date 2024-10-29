f2fa600581ba35903a2524536cb163537d0ff0ff
Shivanjali Karande <shivanjalikarande1@gmail.com>
2024-10-29 11:09:31 +0530

    Image added for product not found

--- script/products.js
+++ script/products.js
@@ -26,9 +26,25 @@ function displayProducts(products) {
             productsSection.innerHTML = ""; 
       }
       
-      if(products == ''){
-            productsSection.innerHTML = "<h1>No Product Found </h1>"
-      }
+      // if (products == '') {
+      //       productsSection.innerHTML = `
+      //           <div style="text-align: center; display: flex; flex-direction: column; align-items: center;">
+      //               <h1 style="color: pink; text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1); margin: 0;">No Product Found</h1>
+      //               <img src="images/product-not-found.png" alt="No products available" style="width: 25%; height: auto; margin: 5% 0;"> <!-- Adjusted image size -->
+      //               <p style="font-size:large; margin-top: 0.2%;">We're sorry, but it seems we can't find the product you're looking for. Try searching for another flavor!</p>
+      //           </div>
+      //       `;
+      //   }
+      if (products == '') {
+            productsSection.innerHTML = `
+                <div class="no-product-container">
+                    <h1 class="no-product-heading">No Product Found</h1>
+                    <img src="images/product-not-found.png" alt="No products available" class="no-product-image">
+                    <p class="no-product-text">We're sorry, but it seems we can't find the product you're looking for. Try searching for another flavor!</p>
+                </div>
+            `;
+        }
+
       products.forEach((product) => {
             let box = document.createElement("div");
             let flavorName = flavor_list.find((f) => f.id === product.flavor_id)?.name;
